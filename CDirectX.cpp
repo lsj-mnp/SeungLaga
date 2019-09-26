6f09f1a686f8e3adf21cf2e2a9a082b7cb317c05
lsj-mnp <53816963+lsj-mnp@users.noreply.github.com>
2019-09-26 12:18:04 +0900

    CDirectX is implemented in CDirectX.cpp

---
+++ CDirectX.cpp
@@ -0,0 +1,26 @@
+#include "CDirectX.h"
+
+void CDirectX::Create(int nShowCmd, WNDPROC WndProc, const char* WindowName, int Width, int Height)
+{
+	WNDCLASSEX wnd_cls{};
+	wnd_cls.cbClsExtra = 0;
+	wnd_cls.cbSize = sizeof(WNDCLASSEX);
+	wnd_cls.cbWndExtra = 0;
+	wnd_cls.hbrBackground = (HBRUSH)GetStockObject(GRAY_BRUSH);
+	wnd_cls.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wnd_cls.hIcon = wnd_cls.hIconSm = LoadIcon(nullptr, IDI_APPLICATION);
+	wnd_cls.hInstance = m_hInstance;
+	wnd_cls.lpfnWndProc = WndProc;
+	wnd_cls.lpszClassName = "Practice";
+	wnd_cls.lpszMenuName = nullptr;
+	wnd_cls.style = CS_HREDRAW | CS_VREDRAW;
+
+	RegisterClassEx(&wnd_cls);
+
+	m_hWnd = CreateWindowEx(0, "Practice", WindowName, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, Width, Height, nullptr, nullptr, m_hInstance, nullptr);
+
+	ShowWindow(m_hWnd, nShowCmd);
+
+	UpdateWindow(m_hWnd);
+}
--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 		}
 		else
 		{
-
+			
 		}
 	}
 
